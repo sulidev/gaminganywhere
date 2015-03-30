d6e791e0b582ad30edef615df30f446e730686c0
sulidev <suliwannabeadeveloper@gmail.com>
2015-03-30 22:06:23 +0700

    changing directx SDK path

--- ga/module/adaptive-stream/adaptive-stream.h
+++ ga/module/adaptive-stream/adaptive-stream.h
@@ -6,7 +6,9 @@
 #ifndef __ADAPTIVE_H__
 #define __ADAPTIVE_H__
 
-__declspec(dllexport) void adaptive_add_sink(RTPSink* sink);
-__declspec(dllexport) void adaptive_remove_sink(RTPSink* sink);
+#include "ga-module.h"
+
+MODULE MODULE_EXPORT void adaptive_add_sink(RTPSink* sink);
+MODULE MODULE_EXPORT void adaptive_remove_sink(RTPSink* sink);
 
 #endif
